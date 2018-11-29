bb36afa4e1b64ee37c559f989baf6fd05fc947ca
Martijn Keesmaat <martijnpk@ziggo.nl>
2018-11-29 20:24:09 +0100

    Added img bg

--- morph.js
+++ morph.js
@@ -1,14 +1,17 @@
-var tl = new TimelineMax({ repeat: -1, repeatDelay: 4, delay: 2 });
+var tl = new TimelineMax({ repeat: -1, repeatDelay: 1, delay: 2 });
 
 tl.to('#a', 1, {
-  morphSVG: '#a',
-  shapeIndex: 1
+  morphSVG: '#a'
 })
   .to('#a', 1, {
-    morphSVG: '#x',
-    shapeIndex: 1
+    morphSVG: '#x'
   })
   .to('#a', 1, {
-    morphSVG: '#a',
-    shapeIndex: 1
+    morphSVG: '#x'
+  })
+  .to('#a', 1, {
+    morphSVG: '#x'
+  })
+  .to('#a', 1, {
+    morphSVG: '#a'
   });
